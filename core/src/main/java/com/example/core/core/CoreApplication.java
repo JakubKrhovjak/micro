194124979f2bbc4a69456896260069332b446f73
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2021-04-25 09:40:32 +0200

    first commit

---
+++ core/src/main/java/com/example/core/core/CoreApplication.java
@@ -0,0 +1,23 @@
+package com.example.core.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+    @GetMapping
+    public Mono<String> ok() {
+        return Mono.just("ok");
+    }
+
+}
---
+++ core/src/test/java/com/example/core/core/CoreApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.core.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
